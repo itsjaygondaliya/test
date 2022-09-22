03269b4cf9773013a36d6da40d229d57bd1e925e
Jay Gondaliya <jaygondaliya147@gmail.com>
2022-09-22 15:36:28 +0530

    new

---
+++ CONST.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#define PI 3.14
+#define P printf
+
+/*
+	scanf("format_specifiers",Address_of_variable);
+
+	address of operator = & (&varName)
+*/
+main()
+{
+   int a,b;
+   clrscr();
+   P("");
+   P("1234567\t9");
+   getch();
+}
